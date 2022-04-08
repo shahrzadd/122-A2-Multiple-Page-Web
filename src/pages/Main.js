f11efc4be238cd9ed0982e4765f5fb0c3bcec82f
shahrzadd <shahrzadd@gmail.com>
2022-04-08 14:00:53 -0400

    Initial Commit

---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import {Button} from "react-bootstrap";
+
+const topBanner = require("../assets/Banner-Top.png");
+const midBanner = require("../assets/Banner-2.png");
+const sideBanner = require("../assets/Banner-Middle.png");
+
+const Main = () => {
+	return (
+	);
+};
+
+
+export default Main;
